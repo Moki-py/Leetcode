29a456aa1995651d6c5cce634bae8cafc8d706b4
Mikhail Snesarev <Rickeymills15@gmail.com>
2024-04-01 20:12:11 +0200

    Add class SolutionSearchInsertPosition (35. Search Insert Position)

--- solutions/nums/easy.py
+++ solutions/nums/easy.py
@@ -6,3 +6,21 @@ class LengthOfLastWord:
     def is_palindrome(self, x: int) -> bool:
         """Solution using string conversion"""
         return str(x) == str(x)[::-1]
+
+
+class SolutionSearchInsertPosition:
+    """ 35. Search Insert Position """
+    def search_insert(self, nums: list[int], target: int) -> int:
+        """ Binary search solution """
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return left
